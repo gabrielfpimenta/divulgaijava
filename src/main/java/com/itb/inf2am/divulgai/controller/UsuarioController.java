868516b381d74b15cdbe7f7c7bd8a61e7f616561
SamuelPS0 <rm93980@estudante.fieb.edu.br>
2026-04-10 11:04:12 -0300

    Ajuste controller usuario

--- src/main/java/com/itb/inf2am/divulgai/controller/UsuarioController.java
+++ src/main/java/com/itb/inf2am/divulgai/controller/UsuarioController.java
@@ -1,139 +1,163 @@
 package com.itb.inf2am.divulgai.controller;
 
-
-// No spring é comum adicionarmos anotações (annotation) em classes, atributos e métodos
-// Uma annotation é uma forma de adicionar informações (metadados) ao seu código que podem
-// ser interpretadas pelo compilador ou em tempo de execução por ferramentas e frameworks
-// como o Spring
-// Uma annotation é uma palavra iniciada com '@' que você coloca acima de uma class, método,
-// atributo ou parâmetro, para dar instruções extras ao Java ou a algum framework sobre como
-// aquele elemento deve ser tratado.
-
-import com.itb.inf2am.divulgai.model.entity.Usuario;
 import com.itb.inf2am.divulgai.model.entity.Usuario;
 import com.itb.inf2am.divulgai.model.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-        import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/Usuario")
 public class UsuarioController {
 
- @Autowired
- private UsuarioService usuarioService;
+    @Autowired
+    private UsuarioService usuarioService;
 
- @GetMapping
- public ResponseEntity<List<Usuario>> findAll() {
+    @GetMapping
+    public ResponseEntity<List<Usuario>> findAll() {
+        return ResponseEntity.ok(usuarioService.findAll());
+    }
 
-  return ResponseEntity.ok(usuarioService.findAll());
- }
+    @PostMapping
+    public Usuario create(@RequestBody Usuario usuario) {
+        return usuarioService.save(usuario);
+    }
 
- // @RequestBody : Corpo da Requisição ( Recebendo um objeto JSON )
- // RespondeEntity: Toda resposta HTTP (status, cabeçalhos e corpo), aqui temos mais controle sobre o que é devolvido para o cliente
- // 1. Status HTTP ( 200 ok, 201 CREATED, 404 NOT FOUND etc...)
- // 2. Headers: ( cabeçalhos extras, como Location, Authorization etc...)
- // 3. Body: ( o objeto que será convertido em JSON/XML para o cliente )
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> listarUsuarioPorId(@PathVariable String id) {
+        try {
+            Long usuarioId = Long.parseLong(id);
+            Usuario usuario = usuarioService.findById(usuarioId);
+            return ResponseEntity.ok(usuario);
 
- @PostMapping
- public Usuario create(@RequestBody Usuario usuario) {
-  return usuarioService.save(usuario); // chama o service
- }
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().body(
+                    Map.of(
+                            "status", 400,
+                            "error", "Bad Request",
+                            "message", "O id informado não é válido: " + id
+                    )
+            );
 
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(404).body(
+                    Map.of(
+                            "status", 404,
+                            "error", "Not Found",
+                            "message", "Usuario não encontrado com o id " + id
+                    )
+            );
+        }
+    }
 
- @GetMapping("/{id}")
- public ResponseEntity<Object> listarUsuarioPorId(@PathVariable String id) {
-  try {
-   return ResponseEntity.ok(usuarioService.findById(Long.parseLong(id)));
-  } catch (NumberFormatException e) {
-   return ResponseEntity.badRequest().body(
-           Map.of(
-                   "status", 400,
-                   "error", "Bad Request",
-                   "message", "O id informado não é válido: " + id
-           )
-   );
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> atualizarUsuario(
+            @PathVariable String id,
+            @RequestBody Usuario usuario
+    ) {
+        try {
+            Long usuarioId = Long.parseLong(id);
+            Usuario usuarioExistente = usuarioService.findById(usuarioId);
 
+            usuarioExistente.setNome(usuario.getNome());
+            usuarioExistente.setEmail(usuario.getEmail());
+            usuarioExistente.setSenha(usuario.getSenha());
+            usuarioExistente.setNivelAcesso(usuario.getNivelAcesso());
+            usuarioExistente.setPs_01(usuario.getPs_01());
+            usuarioExistente.setPs_02(usuario.getPs_02());
+            usuarioExistente.setFoto(usuario.getFoto());
+            usuarioExistente.setStatusUsuario(usuario.getStatusUsuario());
 
-  } catch (RuntimeException e) {
-   return ResponseEntity.status(404).body(
-           Map.of(
-                   "status", 404,
-                   "error", "Not Found",
-                   "message", "Usuario não encontrada com o id " + id
-           )
+            Usuario usuarioAtualizado = usuarioService.save(usuarioExistente);
 
-   );
+            return ResponseEntity.ok(usuarioAtualizado);
 
-  }
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().body(
+                    Map.of(
+                            "status", 400,
+                            "error", "Bad Request",
+                            "message", "O id informado não é válido: " + id
+                    )
+            );
 
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(404).body(
+                    Map.of(
+                            "status", 404,
+                            "error", "Not Found",
+                            "message", "Usuario não encontrado com o id " + id
+                    )
+            );
+        }
+    }
 
- }
+    @PutMapping("/{id}/foto")
+    public ResponseEntity<Object> atualizarFotoUsuario(
+            @PathVariable String id,
+            @RequestBody Map<String, String> body
+    ) {
+        try {
+            Long usuarioId = Long.parseLong(id);
+            Usuario usuarioExistente = usuarioService.findById(usuarioId);
 
- @PutMapping("/{id}")
- public ResponseEntity<Object> atualizarUsuario(@PathVariable String id, @RequestBody Usuario usuario) {
-  try {
-   Long usuarioId = Long.parseLong(id);
-   Usuario usuarioExistente = usuarioService.findById(usuarioId); // já lança exceção se não achar
+            String foto = body.get("foto");
+            usuarioExistente.setFoto(foto);
 
-   // Atualiza todos os campos permitidos
-   usuarioExistente.setNome(usuario.getNome());
-   usuarioExistente.setEmail(usuario.getEmail());
-   usuarioExistente.setSenha(usuario.getSenha());
-   usuarioExistente.setNivelAcesso(usuario.getNivelAcesso());
-   usuarioExistente.setStatusUsuario(usuario.getStatusUsuario());
+            Usuario usuarioAtualizado = usuarioService.save(usuarioExistente);
 
-   Usuario usuarioAtualizado = usuarioService.save(usuarioExistente);
+            return ResponseEntity.ok(usuarioAtualizado);
 
-   return ResponseEntity.ok(usuarioAtualizado);
-  } catch (NumberFormatException e) {
-   return ResponseEntity.badRequest().body(
-           Map.of(
-                   "status", 400,
-                   "error", "Bad Request",
-                   "message", "O id informado não é válido: " + id
-           )
-   );
-  } catch (RuntimeException e) {
-   return ResponseEntity.status(404).body(
-           Map.of(
-                   "status", 404,
-                   "error", "Not Found",
-                   "message", "Usuario não encontrado com o id " + id
-           )
-   );
-  }
- }
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().body(
+                    Map.of(
+                            "status", 400,
+                            "error", "Bad Request",
+                            "message", "O id informado não é válido: " + id
+                    )
+            );
 
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(404).body(
+                    Map.of(
+                            "status", 404,
+                            "error", "Not Found",
+                            "message", "Usuario não encontrado com o id " + id
+                    )
+            );
+        }
+    }
 
- @DeleteMapping("/{id}")
- public ResponseEntity<Object> excluirUsuario(@PathVariable String id) {
-  try {
-   Long usuarioId = Long.parseLong(id);
-   usuarioService.delete(usuarioId); // chama o service
-   return ResponseEntity.ok(Map.of("message", "Usuario deletada com sucesso"));
-  } catch (NumberFormatException e) {
-   return ResponseEntity.badRequest().body(
-           Map.of(
-                   "status", 400,
-                   "error", "Bad Request",
-                   "message", "O id informado não é válido: " + id
-           )
-   );
-  } catch (RuntimeException e) {
-   return ResponseEntity.status(404).body(
-           Map.of(
-                   "status", 404,
-                   "error", "Not Found",
-                   "message", "Usuario não encontrada com o id " + id
-           )
-   );
-  }
- }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> excluirUsuario(@PathVariable String id) {
+        try {
+            Long usuarioId = Long.parseLong(id);
+            usuarioService.delete(usuarioId);
+
+            return ResponseEntity.ok(
+                    Map.of("message", "Usuario deletado com sucesso")
+            );
+
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().body(
+                    Map.of(
+                            "status", 400,
+                            "error", "Bad Request",
+                            "message", "O id informado não é válido: " + id
+                    )
+            );
+
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(404).body(
+                    Map.of(
+                            "status", 404,
+                            "error", "Not Found",
+                            "message", "Usuario não encontrado com o id " + id
+                    )
+            );
+        }
+    }
 }
-
